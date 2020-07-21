bfe3504d7b5f38d471347596f924f71158b2dc40
penumbra23 <glbranimir@gmail.com>
2020-07-21 23:23:07 +0200

    Add DNS service as hosted service

---
+++ SharpNS/HostedServices/DnsHostedService.cs
@@ -0,0 +1,30 @@
+﻿using DNS.Server;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharpNS.HostedServices
+{
+    public class DnsHostedService : IHostedService
+    {
+        public DnsHostedService()
+        {
+            MasterFile masterFile = new MasterFile();
+            // TODO: remove testing data
+            masterFile.AddIPAddressResourceRecord("google.com", "12.23.34.45");
+            Server = new DnsServer(masterFile, "8.8.8.8");
+        }
+
+        private DnsServer Server { get; }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await Server.Listen();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- SharpNS/Program.cs
+++ SharpNS/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SharpNS.HostedServices;
 
 namespace SharpNS
 {
@@ -21,6 +17,10 @@ namespace SharpNS
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<DnsHostedService>();
                 });
     }
 }
