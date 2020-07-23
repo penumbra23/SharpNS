1d6c3b3f785953b5aa3ec1a00891276bcddaa3ac
penumbra23 <glbranimir@gmail.com>
2020-07-24 00:46:11 +0200

    Add base class for controllers

---
+++ SharpNS/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SharpNS.Models.Database;
+
+namespace SharpNS.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public BaseController(ILogger logger, DNSContext context)
+        {
+            Context = context;
+            Logger = logger;
+        }
+
+        protected DNSContext Context { get; set; }
+        protected ILogger Logger { get; set; }
+    }
+}
