055e8f502407ff808c21ccc069dd7d8d6ec79cd0
penumbra23 <glbranimir@gmail.com>
2020-07-24 23:22:12 +0200

    Implement controller logic

--- SharpNS/Controllers/DnsRecordController.cs
+++ SharpNS/Controllers/DnsRecordController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using SharpNS.Convertes;
+using SharpNS.Exceptions;
+using SharpNS.Models.API;
 using SharpNS.Models.Database;
 
 namespace SharpNS.Controllers
@@ -9,14 +13,48 @@ namespace SharpNS.Controllers
     [ApiController]
     public class DnsRecordController : BaseController
     {
-        public DnsRecordController(ILogger<DnsRecordController> logger, DNSContext context) : base(logger, context)
-        {
-        }
+        public DnsRecordController(ILogger<DnsRecordController> logger, DNSContext context) : base(logger, context) { }
 
         [HttpGet]
-        public IActionResult ListDnsRecords()
+        public async Task<IActionResult> ListDnsRecords()
         {
-            return Ok(Context.Records.ToList());
+            return Ok(await Context.Records.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ListDnsRecords([FromRoute] int id)
+        {
+            Record record = await Context.Records.FindAsync(id);
+            if (record == null) throw new ApiException(404, $"Record with id {id} not found.");
+            return Ok(record);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDnsRecord(DnsRecord record)
+        {
+            var entity = await Context.Records.AddAsync(DomainModelConverter.ToDomainModel(record));
+            await Context.SaveChangesAsync();
+            return Ok(entity.Entity);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> ChangeRecordAddress([FromRoute]int id, [FromBody] IpChange changeReq)
+        {
+            Record record = await Context.Records.FindAsync(id);
+            if (record == null) throw new ApiException(404, $"Record with id {id} not found.");
+            record.IpAddress = changeReq.Ip;
+            await Context.SaveChangesAsync();
+            return Ok(record);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecord([FromRoute] int id)
+        {
+            Record record = await Context.Records.FindAsync(id);
+            if (record == null) throw new ApiException(404, $"Record with id {id} not found.");
+            Context.Records.Remove(record);
+            await Context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
