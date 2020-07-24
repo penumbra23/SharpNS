9b013a1fef99fc5f463659c47ffa1ca345756ed1
penumbra23 <glbranimir@gmail.com>
2020-07-24 22:28:29 +0200

    Add API models for records and ip changes

---
+++ SharpNS/Models/API/DnsRecord.cs
@@ -0,0 +1,14 @@
+﻿using DNS.Protocol;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharpNS.Models.API
+{
+    public class DnsRecord : IpChange
+    {
+        [Required(ErrorMessage = "'domain' is a required field.")]
+        public string Domain { get; set; }
+
+        [Required(ErrorMessage = "'type' is a required field.")]
+        public RecordType Type { get; set; }
+    }
+}
---
+++ SharpNS/Models/API/IpChange.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharpNS.Models.API
+{
+    public class IpChange
+    {
+        [Required(ErrorMessage = "'ip' is a required field.")]
+        public string Ip { get; set; }
+    }
+}
